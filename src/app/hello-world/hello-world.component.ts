567af98b3b5e5ec24703c6b0fb5bee6511a9882e
RoseMagura <magurarm@guilford.edu>
2021-01-11 14:13:58 -0500

    Feat: Set up hero service async

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hello-world',
@@ -9,15 +10,21 @@ import { HEROES } from '../mock-heroes';
 })
 export class HelloWorldComponent implements OnInit {
 
-    heroes = HEROES;
+    heroes: Hero[];
+
+    getHeroes(): void {
+        this.heroService.getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
+    }
 
     selectedHero: Hero;
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+      this.getHeroes();
   }
 
 }

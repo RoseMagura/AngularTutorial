98b61a73a00cf16e29a5f6c1f6bfa8c2b7af86f5
RoseMagura <magurarm@guilford.edu>
2021-01-12 11:13:11 -0500

    Feat: Can edit hero name and save

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +12,44 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
-  
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
       this.messageService.add('Hero Service: fetched heroes');
-      return this.http.get<Hero[]>(this.heroesUrl);
+      return this.http.get<Hero[]>(this.heroesUrl)
+        .pipe(
+            tap(_ => this.log('fetched heroes')),
+            catchError(this.handleError<Hero[]>('getHeroes', []))
+        );
   }
 
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // Send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+        tap(_ => this.log(`fetched hero id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+      return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+          tap(_ => this.log(`updated hero id=${hero.id}`)),
+          catchError(this.handleError<any>('updateHero'))
+      );
+  }
+  private handleError<T>(operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+          console.log(error);
+          this.log(`${operation} failed: ${error.message}`);
+          return of(result as T);
+      }
+  }
   constructor(
       private messageService: MessageService,
       private http: HttpClient) { }

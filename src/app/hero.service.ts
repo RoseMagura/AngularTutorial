6cb4ce7b1924353e6833f1a91207c45772152caf
RoseMagura <magurarm@guilford.edu>
2021-01-11 22:25:19 -0500

    Feat: Starting to add httpclient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,15 +3,18 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';  // URL to web api
+  
   getHeroes(): Observable<Hero[]> {
       this.messageService.add('Hero Service: fetched heroes');
-      return of(HEROES);
+      return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
@@ -20,5 +23,12 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id));
   }
 
-  constructor(private messageService: MessageService) { }
-}
+  constructor(
+      private messageService: MessageService,
+      private http: HttpClient) { }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+    }
